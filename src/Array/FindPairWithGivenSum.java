753a511307ce4febeb98b5b2ae55a7aaa65509da
Joy liu <joyliu1218@gmail.com>
2019-01-31 17:16:56 -0800

    Initial Commit
    Initial Commit

---
+++ src/Array/FindPairWithGivenSum.java
@@ -0,0 +1,22 @@
+package Array;
+
+/*
+ * From https://www.techiedelight.com/find-pair-with-given-sum-array/
+ * 
+ * Given an unsorted array of integers, find a pair with given sum in it.
+ * 
+ * For example:
+ * Input: 
+ * arr = [8, 7, 2, 5, 3, 1]
+ * sum = 10
+ * 
+ * Output:
+ * 
+ * Pair found at index 0 and 2 (8 + 2)
+ * or
+ * Pair found at index 1 and 4 (7 + 3)
+ */
+
+public class FindPairWithGivenSum {
+
+}
